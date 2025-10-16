0b75b0b8b2ced85ae5ea226d3c9fb694c42142d6
hina0629 <hina.0629.xxx@gmail.com>
2025-10-16 17:27:11 +0900

    feat : 文章を表示させる機能を追加

--- 1016/01_start/hello.js
+++ 1016/01_start/hello.js
@@ -1,3 +1,10 @@
 // ES6 Code Snippets のプラグインを入れておくと
 // clg ==> console.log()
 console.log('Hello JS!');
+
+var message = 'Nodejsの勉強を始めました'
+console.log(message);
+
+// 動かないプログラム
+// document.createElement('div');
+// document is not find ==> Nodejsにはdocumentが存在しないからエラー
